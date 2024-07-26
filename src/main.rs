a6d17dcd6b424bb3a08d1f15cbf272b5fc29c44e
Guru Das Srinagesh <gurooodas@gmail.com>
2024-07-25 23:09:08 -0700

    shell: Support exit builtin
    Had to refactor the program quite a bit, but got there in the end.
    
    Signed-off-by: Guru Das Srinagesh <gurooodas@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,52 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::{collections::HashMap, process::exit};
 
-fn is_valid(_s: &String) -> bool {
-    false
+enum CmdHandler {
+    Exit(fn(i32)),
+}
+
+fn handle_exit(exit_code: i32) {
+    exit(exit_code);
+}
+
+fn is_valid(builtins: &HashMap<String, CmdHandler>, input: &str) -> bool {
+    return builtins.contains_key(input);
+}
+
+fn process_input(builtins: &HashMap<String, CmdHandler>, input: &String) {
+    let tokens: Vec<&str> = input.split_whitespace().collect();
+
+    if tokens.len() == 0 {
+        return;
+    }
+
+    if !is_valid(&builtins, &tokens[0]) {
+        println!("{}: command not found", input.trim());
+        return;
+    }
+
+    let cmd = builtins.get(&tokens[0] as &str);
+    if let Some(cmd) = cmd {
+        match cmd {
+            CmdHandler::Exit(f) => {
+                if let Ok(code) = tokens[1].parse::<i32>() {
+                    f(code);
+                }
+            }
+        }
+    }
+}
+
+fn initialize_shell_builtins(builtins: &mut HashMap<String, CmdHandler>) {
+    builtins.insert("exit".to_string(), CmdHandler::Exit(handle_exit));
 }
 
 fn main() {
+    let mut builtins: HashMap<String, CmdHandler> = HashMap::new();
+
+    initialize_shell_builtins(&mut builtins);
+
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -15,8 +56,6 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
 
-        if !is_valid(&input) {
-            println!("{}: command not found", input.trim());
-        }
+        process_input(&builtins, &input);
     }
 }
